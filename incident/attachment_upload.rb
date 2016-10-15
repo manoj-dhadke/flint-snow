201f6dd206fab7e1d7c36d5f5c8172ba1d5860f6
anurag-infiverve <anurag.dubey@infiverve.com>
2016-10-15 18:51:07 +0530

    flintbox name changed to flint-snow

---
+++ incident/attachment_upload.rb
@@ -0,0 +1,48 @@
+# begin
+@log.trace("Started executing 'flint-snow:incident:attachment_upload.rb' flintbit...")
+begin
+    # Flintbit Input Parameters
+    @connector_name = @input.get('connector_name') # Name of the ServiceNow Connector
+    if @connector_name.nil?
+       @connector_name = @config.global("flintserve.connector_name")
+      if @connector_name.nil?
+       @connector_name = 'servicenow'
+    end
+   end
+    @action = 'upload-attachment'                     # Contains the name of the operation: list
+    @tableName = 'incident'
+    @sysid = @input.get('sys-id')
+    @filename = @input.get('file-name')
+   # @sysparm_display_value = @input.get('sysparm_display_value')
+
+    @log.info("Flintbit input parameters are, connector name :: #{@connector_name} |action :: #{@action}| tableName :: #{@tableName}")
+
+          response = @call.connector(@connector_name)
+                          .set('action', @action)
+                          .set('table-name', @tableName)
+                          .set('sys-id', @sysid)
+                          .set('file-name', @filename)
+                          .sync
+
+    # ServiceNow Connector Response Meta Parameters
+    response_exitcode = response.exitcode           # Exit status code
+    response_message = response.message             # Execution status message
+
+    # ServiceNow Connector Response Parameters
+    response_body = response.get('body')            # Response body
+    @log.info(response_body)
+    if response_exitcode == 0
+        @log.info("Success in executing servicenow Connector, where exitcode :: #{response_exitcode} | message :: #{response_message}")
+        @output.set('result', response_body)
+        @log.trace("Finished executing 'servicenow' flintbit with success...")
+    else
+        @log.error("Failure in executing servicenow Connector where, exitcode :: #{response_exitcode} | message :: #{response_message}")
+        @output.set('error', response_message)
+        @log.trace("Finished executing 'servicenow' flintbit with error...")
+    end
+rescue Exception => e
+    @log.error(e.message)
+    @output.set('exit-code', 1).set('message', e.message)
+end
+@log.trace("Finished executing 'flint-snow:incident:attachment_upload.rb' flintbit...")
+# end
---
+++ incident/note_process_data.rb
@@ -0,0 +1,20 @@
+@log.info("Starting called flintbit :: flint-snow:incident:note_process_data.rb")
+ 
+ data = @input.get("result")
+ post_data = {}
+ post_data["comments"] = data["comments"].gsub(/[\n]/, ' ')
+ post_data["work_notes"] = data["work_notes"].gsub(/[\n]/, ' ')
+ 
+ 
+ @log.info("result :: #{post_data}")
+ @output.set("data", "#{post_data}")
+
+@log.info("End :: flint-snow:incident:note_process_data.rb")
+
+
+
+
+ #@log.info("result :: #{post_data.class}")
+ #name = post_data.map{|k,v| "#{k}=#{v}"}.join('&')
+ #@log.info("result :: #{name.class}")
+ #@output.set("data", post_data)
