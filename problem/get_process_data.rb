160024e807f970b1211fdaca05e7f3d609a3fa8a
anurag-infiverve <anurag.dubey@infiverve.com>
2016-10-27 18:45:40 +0530

    bug fixed

--- problem/get_process_data.rb
+++ problem/get_process_data.rb
@@ -10,11 +10,11 @@
         end
 
         if ((!data["assigned_to"].nil?) && (!data["assigned_to"]["value"].nil?))
-        data["assigned_to"] = data["assigned_to"]
-        @log.info("ASSIGNED_TO VALUE #{data["assigned_to"]["value"]}")
+        data["assigned_to"] = data["assigned_to"]["value"]
+        #@log.info("ASSIGNED_TO VALUE #{data["assigned_to"]["value"]}")
         #response = @call.bit("flint-snow:problem:list_assigned_to_user.rb").set("sys-id", data["assigned_to"]).sync
         #data["assigned_to"]=response.get("name")
-	@log.info(data["assigned_to"])
+	@log.info("#{data["assigned_to"]}")
       	end
 
       	if ((!data["opened_by"].nil?) && (!data["opened_by"]["value"].nil?))
@@ -112,6 +112,12 @@
         data["rfc"] = data["rfc"]["value"]
         response = @call.bit("flint-snow:problem:list_rfc.rb").set("sys-id", data["rfc"]).sync
         data["rfc"]=response.get("name")
+        end
+        if ((!data["change_request"].nil?) && (!data["change_request"]["value"].nil?))
+	data["change_request"] = data["change_request"]["value"]
+        #response = @call.bit("flint-snow:problem:list_assignmentgroup.rb").set("sys-id", data["assignment_group"]).sync
+        #data["assignment_group"]=response.get("name")
+        @log.info(data["change_request"])
         end
 
         response1 = @call.bit("flint-snow:problem:list_activity.rb").set("sys-id",data["sys_id"]).sync
