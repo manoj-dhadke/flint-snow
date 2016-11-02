28aa4a915f893cc96c500fb4ae2a1c391db6d8d0
anurag-infiverve <anurag.dubey@infiverve.com>
2016-11-02 11:59:05 +0530

    Bug fix

--- change/process_data.rb
+++ change/process_data.rb
@@ -2,34 +2,75 @@ require 'json'
 
 @log.info("Starting flintbit change:process_data.rb")
 data = @input.get("result")
-    
+ # @log.info("Data #{data}")
         post_data = {}
- 	data.each do |post_data|       
+ 	data.each do |data|       
          
-        if ((!post_data["assignment_group"].nil?) && (!post_data["assignment_group"]["value"].nil?))
-	post_data["assignment_group"] = post_data["assignment_group"]["value"]
+        if ((!data["assignment_group"].nil?) && (!data["assignment_group"]["value"].nil?))
+	post_data["assignment_group"] = data["assignment_group"]["value"]
         response = @call.bit("flint-snow:change:list_assignmentgroup.rb").set("sys-id", post_data["assignment_group"]).sync
-           if !response.get("name").nil?
+        if !response.nil?
         post_data["assignment_group"]=response.get("name")
-           end
+        end	
+        end
+        if ((!data["assigned_to"].nil?) && (!data["assigned_to"]["value"].nil?))
+        post_data["assigned_to"] = data["assigned_to"]["value"]
+        response = @call.bit("flint-snow:change:list_assigned_to_user.rb").set("sys-id", post_data["assigned_to"]).sync
+        if !response.nil?
+        post_data["assigned_to"]=response.get("name")
+      	end
+        end
+        if ((!data["caller_id"].nil?) && (!data["caller_id"]["value"].nil?))
+        post_data["caller_id"] = data["caller_id"]["value"]
+        response = @call.bit("flint-snow:change:list_configuration_item.rb").set("sys-id", post_data["cmdb_ci"]).sync
+        if !response.nil?
+        post_data["caller_id"]=response.get("name")
+	end
+        end
+        post_data["priority"] = data["priority"]
+        post_data["short_description"] = data["short_description"]
+	post_data["closed_at"] = data["closed_at"]
+	post_data["sys_updated_on"] = data["sys_updated_on"]
+	post_data["sys_id"] = data["sys_id"]
+	post_data["number"] = data["number"]
+	post_data["sys_updated_by"] = data["sys_updated_by"]
+	post_data["incident_state"] = data["incident_state"]
+	post_data["correlation_id"] = data["correlation_id"]
+	post_data["category"] = data["category"] 
+	post_data["escalation"] = data["escalation"] 
+        
+        @log.info("#{post_data}")
+        @call.bit("flint-snow:change:servicenowurl.rb").set('connector_name', 'http').set('body',post_data).sync 
+end
+@log.info("End :: change:process_data.rb")
+
+
+
+
+
+
+=begin    
+        post_data = {}
+ 	data.each do |data|       
+         
+        if ((!data["assignment_group"].nil?) && (!data["assignment_group"]["value"].nil?))
+	post_data["assignment_group"] = data["assignment_group"]["value"]
+        response = @call.bit("flint-snow:change:list_assignmentgroup.rb").set("sys-id", post_data["assignment_group"]).sync
+        post_data["assignment_group"]=response.get("name")
         end
 
         if ((!post_data["assigned_to"].nil?) && (!post_data["assigned_to"]["value"].nil?))           
         post_data["assigned_to"] = post_data["assigned_to"]["value"]
         @log.info("VALUE :: #{post_data["assigned_to"]}")
         response = @call.bit("flint-snow:change:list_assigned_to_user.rb").set("sys-id", post_data["assigned_to"]).sync
-           if !response.get("name").nil?
         post_data["assigned_to"]=response.get("name")
-      	   end
         end
 
         if ((!post_data["cmdb_ci"].nil?) && (!post_data["cmdb_ci"]["value"].nil?))
         post_data["cmdb_ci"] = post_data["cmdb_ci"]["value"]
         @log.info("VALUE CMDB_CI :: #{post_data["cmdb_ci"]}")
         response = @call.bit("flint-snow:change:list_configuration_item.rb").set("sys-id", post_data["cmdb_ci"]).sync
-           if !response.get("name").nil?
         post_data["cmdb_ci"]=response.get("name")
-           end
         end
 
         post_data["priority"] = post_data["priority"]
@@ -51,13 +92,7 @@ data = @input.get("result")
 
         @log.info("#{post_data}")
         @output.set("data", post_data)
-        
-       @call.bit("flint-snow:change:servicenowurl.rb").set('connector_name', 'http').set('body',post_data).sync 
-end
-@log.info("End :: change:process_data.rb")
-
-       
-      
+=end           
 =begin
 
 
